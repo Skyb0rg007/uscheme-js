e4a0aa840758e834563123b3adcfafe03f8f3bbb
Skye Soss <skyler.soss@gmail.com>
2020-07-14 23:47:55 -0400

    Updates

---
+++ precache-manifest.23931e4da9ec2cef0ca1b47be17f9d3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50bfb20262e82d10fa75b800297585d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5b8a42e6ba54f0f1080",
+    "url": "/static/css/2.86383143.chunk.css"
+  },
+  {
+    "revision": "a5b8a42e6ba54f0f1080",
+    "url": "/static/js/2.58763798.chunk.js"
+  },
+  {
+    "revision": "5e819de06af026d7499024f710cfdda3",
+    "url": "/static/js/2.58763798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1014cdfd7777b216d1d",
+    "url": "/static/js/main.65e825ba.chunk.js"
+  },
+  {
+    "revision": "57cd968b5b74780373b7",
+    "url": "/static/js/runtime-main.adefa0fb.js"
+  }
+]);
